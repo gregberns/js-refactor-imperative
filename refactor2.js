412e290de06fee4edcdd73550d15fef5817b60b3
Greg Berns <gregberns@gmail.com>
2021-05-13 22:35:50 -0700

    closer

--- refactor2.js
+++ refactor2.js
@@ -5,58 +5,48 @@ var doThingsAndStuff3 = x => {
     const arr =
         [...x]
             .reverse()
-            .filter(s => s !== null && s !== "")
-            // string must contain a ' '
-            .filter(s => contains(' ', s))
+            .filter(s => s !== null && s !== "" && contains(' ', s))
             .reduce((agg, str) => {
-                log && console.log(`agg: ${JSON.stringify(agg)}, str: '${JSON.stringify(str)}'`)
-                let res = func3(agg, str, str.indexOf(' ') + 1)
+                let i = func3(func4(str, str.indexOf(' ') + 1), agg)
 
-                let arr = res !== null
-                    ? addAtPosition(agg, res, str)
-                    : agg
-
-                return arr.includes(str) ? arr : addToEnd(arr, str)
+                return i !== null
+                    ? addAtPosition(agg, i === 0 ? 0 : i - 1, str)
+                    : agg.includes(str) ? agg : addToEnd(agg, str)
             }, [])
 
-    //Objective: Remove all items from array, add all items from new list
-    log && console.log(`arr12: ${JSON.stringify(arr)}, x: ${JSON.stringify(x)}`)
+    //To keep the interface exactly the same, this it (almost) the only mutation in the program
     x.splice(0, x.length)
-    log && console.log(`arr13: ${JSON.stringify(arr)}, x: ${JSON.stringify(x)}`)
     x.push.apply(x, [...arr].reverse())
-    log && console.log(`arr14: ${JSON.stringify(arr)}, x: ${JSON.stringify(x)}`)
 };
 
-const func3 = (arr, str, start) => {
-    // This is hard to move to a higher order function because of the exit early and returing of the default arr
+const func3 = (f, arr) => {
     for (let i = 0; i < arr.length; i++) {
-        const ii = i === 0 ? 0 : i - 1
-        var b = func4(str, start, arr[i])
+        var b = f(arr[i])
 
         if (b !== null) {
-            return b ? ii : null
+            return b ? i : null
         }
     }
     return null
 }
-const func4 = (str, start, strItem) => {
+const func4 = (str, start) => (strItem) => {
     var isFound = false;
     var strItr = start
 
     for (var y = 0; y < strItem.length; y++) {
-        const chr = strItem.charCodeAt(y)
+        const chr1 = strItem.charCodeAt(y)
         if (!isFound) {
-            if (chr == 32) {
+            if (chr1 == 32) {
                 isFound = true;
             }
             continue;
         }
-        const temp7 = str.charCodeAt(strItr)
-        if (temp7 === chr) {
+        const chr2 = str.charCodeAt(strItr)
+        if (chr1 === chr2) {
             strItr++;
             continue;
         }
-        return (!temp7 || temp7 < chr)
+        return (!chr2 || chr2 < chr1)
     }
     return null
 }
